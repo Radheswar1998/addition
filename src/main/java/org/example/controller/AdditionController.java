114519b334115ff2ae64a49fda4e78cc38ee8ca1
Radheswar1998 <m.radhes@gmail.com>
2023-06-24 11:22:20 -0500

    changes made

--- src/main/java/org/example/controller/AdditionController.java
+++ src/main/java/org/example/controller/AdditionController.java
@@ -9,8 +9,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdditionController {
 
     @GetMapping("/add")
-    public int addNumbers(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return num1 + num2;
+    public Result addNumbers(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int result = num1 + num2;
+        return new Result(result, num1, num2);
+    }
+
+    public static class Result {
+        private int result;
+        private int num1;
+        private int num2;
+
+        public Result(int result, int num1, int num2) {
+            this.result = result;
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public int getResult() {
+            return result;
+        }
+
+        public int getNum1() {
+            return num1;
+        }
+
+        public int getNum2() {
+            return num2;
+        }
     }
 }
 
